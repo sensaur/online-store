321bf89d35ab3098bb18a623b90bdcf764bdc97f
Ilya Zhigarev <zhigarev@bk.ru>
2022-12-21 11:36:19 +0100

    fix: transfer dev server options to build webpack dev

--- webpack.config.js
+++ webpack.config.js
@@ -39,15 +39,6 @@ const baseConfig = {
         assetModuleFilename: '[name][ext]',
     },
     devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
