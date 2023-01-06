d8d9f04da1d851191d26c450c00e6b9a8a9789b0
Ilya Zhigarev <zhigarev@bk.ru>
2023-01-06 22:22:55 +0100

    feat: add render product cards via js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import './styles/main.scss';
 import getDualSliderWorking from './components/app/getDualSliderWorking';
 import filtersList from './components/app/filters';
-import slider from './components/app/card';
+// import slider from './components/app/card';
 import productData from './productList';
-import { makeLogger } from 'ts-loader/dist/logger';
 
 filtersList();
 
@@ -20,9 +19,8 @@ if (multirangeBlockStock instanceof HTMLElement) {
 }
 
 const bigSizeItemsPics = true;
-const productItem = document.querySelector('.p_wrapper');
 
-export const currentUrl = new URL(window.location.href);
+// export const currentUrl = new URL(window.location.href);
 export const queryParams: { [key: string]: string[] } = {};
 
 window.onload = () => {
@@ -71,53 +69,92 @@ window.onload = () => {
         };
         const filteredByCategory = filterByCategory();
 
-        filteredByCategory.forEach((el) => {
-            const itemEl = document.createElement('div');
-            itemEl.classList.add('p_wrapper');
-            const itemWrapper = document.createElement('div');
-            itemWrapper.classList.add('p_product-img');
-            const image = document.createElement('img');
-            image.src = el.thumbnail;
-            itemEl.appendChild(itemWrapper);
-            itemWrapper.appendChild(image);
-            const productItems = document.getElementById('products__items');
+        if (objectFromCurrenQueryParams.big[0] === 'true') {
+            filteredByCategory.forEach((el) => {
+                const itemEl = document.createElement('div');
+                itemEl.classList.add('p_wrapper');
+                const itemWrapper = document.createElement('div');
+                itemWrapper.classList.add('p_product-img');
+                const image = document.createElement('img');
+                image.src = el.thumbnail;
+                itemEl.appendChild(itemWrapper);
+                itemWrapper.appendChild(image);
+                const productItems = document.getElementById('products__items');
 
-            const productInfo = document.createElement('div');
-            productInfo.classList.add('p_product-info');
-            const productText = document.createElement('div');
-            productText.classList.add('p_product-text');
-            const header = document.createElement('h1');
-            header.innerText = el.title;
-            itemEl.appendChild(productInfo);
-            productInfo.appendChild(productText);
-            productText.appendChild(header);
-            const headerBrand = document.createElement('h2');
-            headerBrand.innerText = el.brand;
-            productText.appendChild(headerBrand);
-            const paragraph = document.createElement('p');
-            paragraph.innerText = `Category: ${el.category.toUpperCase()}
+                if (productItems) {
+                    const productInfo = document.createElement('div');
+                    productInfo.classList.add('p_product-info');
+                    const productText = document.createElement('div');
+                    productText.classList.add('p_product-text');
+                    const header = document.createElement('h1');
+                    header.innerText = el.title;
+                    itemEl.appendChild(productInfo);
+                    productInfo.appendChild(productText);
+                    productText.appendChild(header);
+                    const headerBrand = document.createElement('h2');
+                    headerBrand.innerText = el.brand;
+                    productText.appendChild(headerBrand);
+                    const paragraph = document.createElement('p');
+                    paragraph.innerText = `Category: ${el.category.toUpperCase()}
         Price: €${el.price}
         Rating: ${el.rating}
         Discount: ${el.discountPercentage}
         Stock: ${el.stock}
         `;
-            productText.appendChild(paragraph);
-            const buttonsDiv = document.createElement('div');
-            buttonsDiv.classList.add('p_product-price-btn');
-            const details = document.createElement('button');
-            details.innerText = 'Details';
-            const addToCard = document.createElement('button');
-            addToCard.innerText = 'Add to cart';
-            buttonsDiv.appendChild(details);
-            buttonsDiv.appendChild(addToCard);
-            productInfo.appendChild(buttonsDiv);
-            const stat = document.getElementsByClassName('stat')[0];
-            stat.innerHTML = `Found: ${filteredByCategory.length}`
+                    productText.appendChild(paragraph);
+                    const buttonsDiv = document.createElement('div');
+                    buttonsDiv.classList.add('p_product-price-btn');
+                    const details = document.createElement('button');
+                    details.innerText = 'Details';
+                    const addToCard = document.createElement('button');
+                    addToCard.innerText = 'Add to cart';
+                    buttonsDiv.appendChild(details);
+                    buttonsDiv.appendChild(addToCard);
+                    productInfo.appendChild(buttonsDiv);
+                    const stat = document.getElementsByClassName('stat')[0];
+                    stat.innerHTML = `Found: ${filteredByCategory.length}`;
+                    productItems.appendChild(itemEl);
+                }
+            });
+        } else {
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            productItems.appendChild(itemEl);
-        });
+            filteredByCategory.forEach((el) => {
+                const itemEl = document.createElement('div');
+                itemEl.classList.add('p_sm_wrapper');
+                const itemWrapper = document.createElement('div');
+                itemWrapper.classList.add('p_sm_product-img');
+                const image = document.createElement('img');
+                image.src = el.thumbnail;
+                itemEl.appendChild(itemWrapper);
+                itemWrapper.appendChild(image);
+                const productItems = document.getElementById('products__items_sm');
+                // const productItems = document.getElementById('products__items');
+
+                if (productItems) {
+                    const productInfo = document.createElement('div');
+                    productInfo.classList.add('p_sm_product-info');
+                    const productText = document.createElement('div');
+                    productText.classList.add('p_sm_product-text');
+                    const header = document.createElement('h1');
+                    header.innerText = el.title;
+                    itemWrapper.appendChild(productInfo);
+                    productInfo.appendChild(productText);
+                    productText.appendChild(header);
+                    const buttonsDiv = document.createElement('div');
+                    buttonsDiv.classList.add('p_sm_product-price-btn');
+                    const details = document.createElement('button');
+                    details.innerText = 'Details';
+                    const addToCard = document.createElement('button');
+                    addToCard.innerText = 'Add to cart';
+                    buttonsDiv.appendChild(details);
+                    buttonsDiv.appendChild(addToCard);
+                    productInfo.appendChild(buttonsDiv);
+                    const stat = document.getElementsByClassName('stat')[0];
+                    stat.innerHTML = `Found: ${filteredByCategory.length}`;
+                    productItems.appendChild(itemEl);
+                }
+            });
+        }
     } catch (err) {
         console.log("Issues with Parsing URL Parameter's - " + err);
     }
